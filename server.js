df2615b051f4f7f8617b08b6d7e9397912b534df
Sandro Rodriguez Anchante <sandro661528@gmail.com>
2025-10-15 09:51:20 -0500

    Creacion de la Plantilla HTML para realizar el CRUD en el Fronted y con su Archivo CSS para su estilo

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express')
 
+const cors = require('cors')
+const path = require('path')
+
 const categoriaRoutes = require('./routes/categoriaRoutes')
 const subcategoriaRoutes = require('./routes/subcategoriaRoutes')
 const docenteRoutes = require('./routes/docenteRoutes')
@@ -8,8 +11,20 @@ const cursoRouter = require('./routes/cursoRoutes')
 const app = express()
 const PORT = process.env.PORT || 3000 //Puerto de la App
 
+app.use(cors({
+  origin: '*',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true
+}))
+
 //Comunicación se realizará JSON
 app.use(express.json())
+
+//Servir los documentos HTML, CSS, JS
+app.use(express.static(path.join(__dirname, 'public')))
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'cursos.html'))
+})
 
 //Rutas
 app.use('/api/categorias', categoriaRoutes)
